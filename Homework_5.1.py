934fe960db0717bb297349bc38ce9298dccba894
Soulive <50267461+Soulive@users.noreply.github.com>
2019-05-27 16:03:09 +0200

    Add files via upload

---
+++ Homework_5.1.py
@@ -0,0 +1,15 @@
+# let's create a function that will take two numbers as parameters and add them together.
+# Call this function many times in your program, each time with different parameters.
+
+print ("Input two numbers, which should be added together")
+
+def number_calculator (number1, number2):
+    solution = number1 + number2
+    return solution
+
+print (number_calculator(20,15))
+print (number_calculator (155, 155))
+print (number_calculator (500, 499))
+
+
+
